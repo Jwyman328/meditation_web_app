7c538e57eea3767ef4f246abc055785118d48bc6
Joseph Wyman <jwymandev@gmail.com>
2020-05-04 19:30:40 -0400

    Add homepage and nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { BackgroundImageComponent } from './components/background-image/background-image.component';
+import { PageHeaderComponent } from './components/page-header/page-header.component';
+import { MainNavBarComponent } from './components/nav-bars/main-nav-bar/main-nav-bar.component';
+import { NavLinkComponent } from './components/nav-bars/nav-link/nav-link.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    BackgroundImageComponent,
+    PageHeaderComponent,
+    MainNavBarComponent,
+    NavLinkComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/homepage/homepage.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.scss'],
+})
+export class HomepageComponent implements OnInit {
+  backgroundImageUrl =
+    'https://images.pexels.com/photos/814499/pexels-photo-814499.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
+    titleHeaderText = "Welcome to the Meditation Homepage"
+  
+    constructor() {}
+
+  ngOnInit(): void {}
+}
