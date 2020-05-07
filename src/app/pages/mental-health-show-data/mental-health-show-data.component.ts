fe538a45c7515272aafd86ce1d73c29b5d0b7ea7
Joseph Wyman <jwymandev@gmail.com>
2020-05-07 16:09:24 -0400

    add get graph data by http

--- src/app/pages/mental-health-show-data/mental-health-show-data.component.ts
+++ src/app/pages/mental-health-show-data/mental-health-show-data.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { EmotionDataService } from '../../services/http-requests/emotion-data.service';
+import { UserAuthDataService } from '../../services/userData/user-auth-data.service';
 
 @Component({
   selector: 'app-mental-health-show-data',
   templateUrl: './mental-health-show-data.component.html',
-  styleUrls: ['./mental-health-show-data.component.scss']
+  styleUrls: ['./mental-health-show-data.component.scss'],
 })
 export class MentalHealthShowDataComponent implements OnInit {
-
-  constructor() { }
+  backgroundImageUrl =
+    'https://images.pexels.com/photos/1200225/pexels-photo-1200225.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500';
+  constructor(
+    private emotionDataService: EmotionDataService,
+    private userAuthDataService: UserAuthDataService
+  ) {}
 
   ngOnInit(): void {
+    //makae a request and get the data to display
+    this.emotionDataService
+      .getAllEmotionData(this.userAuthDataService.token)
+      .subscribe(
+        (emotionDataResponse) => {
+          console.log(emotionDataResponse, 'dr');
+          this.emotionDataService.setPastMonthEmotionData(
+            emotionDataResponse[0]['moods_range'][0]
+          );
+          this.emotionDataService.setPastWeekEmotionData(
+            emotionDataResponse[0]['moods_range'][1]
+          );
+          this.emotionDataService.setDateValues(emotionDataResponse[0]['moods_range'][2]);
+          this.emotionDataService.handleGetRequestSuccess();
+        },
+        (error) => {
+          console.log(error);
+          this.emotionDataService.handleGetRequestError();
+        }
+      );
   }
-
 }
---
+++ src/app/services/http-requests/emotion-data.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmotionDataService {
+
+  isError = false;
+  isSuccess = false;
+  isLoading = false;
+  pastMonthData:number[];
+  pastWeekData: number[];
+  today:string;
+  dateAWeekAgo: string;
+  dateAMonthAgo:string;
+  constructor(private http:HttpClient) { }
+
+  /**
+   * Fetch emotion data that will include past thirty days 
+   * and past week.
+   */
+  getAllEmotionData(token){
+    this.handleGetRequestSent();
+    return this.http.get('http://intense-gorge-29567.herokuapp.com/Journal/last_week_moods/str',
+    {headers: new HttpHeaders({ Authorization: `JWT ${token}`,'Content-Type': 'application/json'})})
+  }
+
+  setPastMonthEmotionData(pastMonthData){
+    this.pastMonthData = pastMonthData;
+  }
+  setPastWeekEmotionData(pastWeekData){
+    this.pastWeekData = pastWeekData
+  }
+
+  setDateValues(dateArray:string[]){
+    this.today = dateArray[0];
+    this.dateAWeekAgo= dateArray[1];
+    this.dateAMonthAgo = dateArray[2];
+  }
+
+  handleGetRequestSent(){
+    this.isLoading = true;
+    this.isSuccess = false;
+    this.isError = false;
+  }
+
+  handleGetRequestSuccess(){
+    this.isError = false;
+    this.isSuccess = true;
+    this.isLoading = false;
+  }
+
+  handleGetRequestError(){
+    this.isError = true;
+    this.isSuccess = false;
+    this.isLoading = false;
+  }
+}
