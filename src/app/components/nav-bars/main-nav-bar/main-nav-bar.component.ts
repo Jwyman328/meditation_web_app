56929a10efdf02da2b610780b1b72733303a0812
Joseph Wyman <jwymandev@gmail.com>
2020-05-08 09:06:14 -0400

    Add nav header to all pages with changing colors

--- src/app/components/nav-bars/main-nav-bar/main-nav-bar.component.ts
+++ src/app/components/nav-bars/main-nav-bar/main-nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-main-nav-bar',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-nav-bar.component.scss']
 })
 export class MainNavBarComponent implements OnInit {
-
+  @Input('isMeditationContainer') isMeditationContainer  = false;
   constructor() { }
 
   ngOnInit(): void {
