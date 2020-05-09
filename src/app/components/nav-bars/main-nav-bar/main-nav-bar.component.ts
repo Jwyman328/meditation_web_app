e07efb2baa8622a2366b36bb0a37448f10ff77d6
Joseph Wyman <jwymandev@gmail.com>
2020-05-09 15:35:20 -0400

    Set min size for navigation button

--- src/app/components/nav-bars/main-nav-bar/main-nav-bar.component.ts
+++ src/app/components/nav-bars/main-nav-bar/main-nav-bar.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit, Input, ViewChild, ElementRef,ChangeDetectorRef, AfterViewInit, OnChanges, AfterContentChecked } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewChild,
+  ElementRef,
+  ChangeDetectorRef,
+  AfterViewInit,
+  OnChanges,
+  AfterContentChecked,
+} from '@angular/core';
 import { UserAuthDataService } from '../../../services/userData/user-auth-data.service';
 
 @Component({
@@ -7,34 +17,37 @@ import { UserAuthDataService } from '../../../services/userData/user-auth-data.s
   styleUrls: ['./main-nav-bar.component.scss'],
 })
 export class MainNavBarComponent implements OnInit {
-  @ViewChild('navContainerElement') navContainerElement:ElementRef;
+  @ViewChild('navContainerElement') navContainerElement: ElementRef;
   @Input('isMeditationContainer') isMeditationContainer = false;
   mobileHeader;
   isHeaderOpen = true;
-  width:number;
-  constructor(private userAuthDataService: UserAuthDataService, private cdr:ChangeDetectorRef) {}
+  width: number;
+  constructor(
+    private userAuthDataService: UserAuthDataService,
+    private cdr: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {}
   handleLogout() {
     this.userAuthDataService.logOutUser();
   }
 
-  ngAfterViewInit(){
-     console.log( this.navContainerElement.nativeElement.offsetWidth, 'wd');
-   this.width=this.navContainerElement.nativeElement.offsetWidth;
-   if(this.navContainerElement.nativeElement.offsetWidth < 450){
-     this.mobileHeader = true;
-   }else{
-     this.mobileHeader = false;
-   }
-   this.cdr.detectChanges()
+  ngAfterViewInit() {
+    this.setHeaderBasedOffPageWidth()
   }
 
+  openCloseHeader() {
+    this.isHeaderOpen = !this.isHeaderOpen;
+    this.cdr.detectChanges();
+  }
 
-
-  openCloseModal(){
-      this.isHeaderOpen = !this.isHeaderOpen
-      this.cdr.detectChanges()
-
+  setHeaderBasedOffPageWidth(){
+    this.width = this.navContainerElement.nativeElement.offsetWidth;
+    if (this.navContainerElement.nativeElement.offsetWidth < 450) {
+      this.mobileHeader = true;
+    } else {
+      this.mobileHeader = false;
+    }
+    this.cdr.detectChanges();
   }
 }
