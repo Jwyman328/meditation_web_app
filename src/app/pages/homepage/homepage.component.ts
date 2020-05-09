5996a70391723e697b435754add4547155da7e35
Joseph Wyman <jwymandev@gmail.com>
2020-05-09 09:14:16 -0400

    add navigate to add journal to homepage

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -9,7 +9,6 @@ import { Component, OnInit } from '@angular/core';
 export class HomepageComponent implements OnInit {
   backgroundImageUrl =
     'https://images.pexels.com/photos/814499/pexels-photo-814499.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
-    titleHeaderText = "Welcome to the Meditation Homepage"
 
     constructor() {}
 
