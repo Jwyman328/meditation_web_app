9f7a239426878ecb05acfc9d705f2f79c258ef83
Joseph Wyman <jwymandev@gmail.com>
2020-05-09 11:57:59 -0400

    Make Mobile header and responsive header

--- src/app/components/nav-bars/main-nav-bar/main-nav-bar.component.ts
+++ src/app/components/nav-bars/main-nav-bar/main-nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef,ChangeDetectorRef, AfterViewInit, OnChanges, AfterContentChecked } from '@angular/core';
 import { UserAuthDataService } from '../../../services/userData/user-auth-data.service';
 
 @Component({
@@ -7,11 +7,34 @@ import { UserAuthDataService } from '../../../services/userData/user-auth-data.s
   styleUrls: ['./main-nav-bar.component.scss'],
 })
 export class MainNavBarComponent implements OnInit {
+  @ViewChild('navContainerElement') navContainerElement:ElementRef;
   @Input('isMeditationContainer') isMeditationContainer = false;
-  constructor(private userAuthDataService: UserAuthDataService) {}
+  mobileHeader;
+  isHeaderOpen = true;
+  width:number;
+  constructor(private userAuthDataService: UserAuthDataService, private cdr:ChangeDetectorRef) {}
 
   ngOnInit(): void {}
   handleLogout() {
     this.userAuthDataService.logOutUser();
+  }
+
+  ngAfterViewInit(){
+     console.log( this.navContainerElement.nativeElement.offsetWidth, 'wd');
+   this.width=this.navContainerElement.nativeElement.offsetWidth;
+   if(this.navContainerElement.nativeElement.offsetWidth < 450){
+     this.mobileHeader = true;
+   }else{
+     this.mobileHeader = false;
+   }
+   this.cdr.detectChanges()
+  }
+
+
+
+  openCloseModal(){
+      this.isHeaderOpen = !this.isHeaderOpen
+      this.cdr.detectChanges()
+
   }
 }
